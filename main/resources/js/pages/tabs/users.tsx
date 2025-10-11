fa553a75424dc6698dadbfc98212e36f94ef4c19
Rexshimura <mortpauljpm03@gmail.com>
2025-10-12 06:34:20 +0800

    initial users page

---
+++ main/resources/js/pages/tabs/users.tsx
@@ -0,0 +1,31 @@
+import { PlaceholderPattern } from "@/components/ui/placeholder-pattern";
+import AppLayout from '@/layouts/app-layout';
+import { users } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Users',
+        href: users().url,
+    },
+];
+
+export default function Users() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Users" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+
+                <h1 className="text-2xl font-bold">User Management</h1>
+
+                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="absolute inset-0 flex items-center justify-center text-lg font-medium">
+                        Users Data Placeholder
+                    </div>
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
