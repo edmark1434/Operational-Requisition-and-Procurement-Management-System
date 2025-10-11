3707e2d8026113f34e351702229fb59ba9b227b0
edmark1434 <edmarktalingting0@gmail>
2025-10-11 21:15:45 +0800

    added migration

--- main/database/migrations/0001_01_01_000000_create_users_table.php
+++ main/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+       Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name', 100)->unique();
+            $table->string('password', 255);
+            $table->dateTime('created_at');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ main/database/migrations/2025_10_11_122903_requisition.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requisition', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at')->nullable();
+            $table->enum('status', ['PENDING', 'REJECTED', 'APPROVED', 'DELIVERED', 'RECEIVED']);
+            $table->string('remarks')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requisition');
+    }
+};
