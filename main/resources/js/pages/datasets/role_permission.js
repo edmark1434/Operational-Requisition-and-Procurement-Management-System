953fcbe5a2d12a3c3a3686d82d07198f559c2598
Rexshimura <mortpauljpm03@gmail.com>
2025-10-13 06:56:20 +0800

    dataset role_perm

---
+++ main/resources/js/pages/datasets/role_permission.js
@@ -0,0 +1,24 @@
+const rolePermissions = [
+    {
+        RP_ID: 6001,
+        ROLE_ID: 8001,
+        PERM_ID: 4001
+    },
+    {
+        RP_ID: 6002,
+        ROLE_ID: 8001,
+        PERM_ID: 4002
+    },
+    {
+        RP_ID: 6003,
+        ROLE_ID: 8002,
+        PERM_ID: 4001
+    },
+    {
+        RP_ID: 6004,
+        ROLE_ID: 8002,
+        PERM_ID: 4003
+    }
+];
+
+export default rolePermissions;
