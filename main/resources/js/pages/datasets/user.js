eba46da8daa9df31b608930371308c0a19cb8912
Rexshimura <mortpauljpm03@gmail.com>
2025-10-13 06:56:12 +0800

    dataset user

---
+++ main/resources/js/pages/datasets/user.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        US_ID: 2001,
+        NAME: "john_doe",
+        PASSWORD: "user123",
+        DATE_CREATED: "2024-01-01 00:00:00"
+    },
+    {
+        US_ID: 2002,
+        NAME: "maria_g",
+        PASSWORD: "user456",
+        DATE_CREATED: "2024-01-02 00:00:00"
+    },
+    {
+        US_ID: 2003,
+        NAME: "david_j",
+        PASSWORD: "user789",
+        DATE_CREATED: "2024-01-03 00:00:00"
+    }
+];
+
+export default users;
