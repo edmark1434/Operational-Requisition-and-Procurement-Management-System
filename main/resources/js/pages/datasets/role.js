8e81a301b25c9f94a570298802c913343f7f49d1
Rexshimura <mortpauljpm03@gmail.com>
2025-10-13 06:56:25 +0800

    dataset role

---
+++ main/resources/js/pages/datasets/role.js
@@ -0,0 +1,19 @@
+const roles = [
+    {
+        RO_ID: 8001,
+        NAME: "Requester",
+        DESCRIPTION: "Can view items and create requisitions"
+    },
+    {
+        RO_ID: 8002,
+        NAME: "Approver",
+        DESCRIPTION: "Can view items and approve/decline requisitions"
+    },
+    {
+        RO_ID: 8003,
+        NAME: "Administrator",
+        DESCRIPTION: "Full system access and user management"
+    }
+];
+
+export default roles;
