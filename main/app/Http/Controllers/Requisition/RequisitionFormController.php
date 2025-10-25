11a03b939098b48e5b78b6180c26ba0e19f2c7c6
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2025-10-26 05:48:50 +0800

    Moved controllers

---
+++ main/app/Http/Controllers/Requisition/RequisitionFormController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Requisition;
+
+use App\Http\Controllers\Controller;
+
+class RequisitionFormController extends Controller
+{
+    //
+}
---
+++ main/app/Http/Controllers/Requisition/RequisitionStoreController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Requisition;
+
+use App\Http\Controllers\Controller;
+
+class RequisitionStoreController extends Controller
+{
+    //
+}
