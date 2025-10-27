a2dd9fd7ca9e7e6ec195c8f3bf520e87bfc58a7a
Rexshimura <mortpauljpm03@gmail.com>
2025-10-28 05:03:11 +0800

    default role

--- main/resources/js/pages/datasets/role.js
+++ main/resources/js/pages/datasets/role.js
@@ -1,18 +1,23 @@
 const roles = [
     {
         RO_ID: 8001,
-        NAME: "Requester",
-        DESCRIPTION: "Can view items and create requisitions"
+        NAME: "Admin",
+        DESCRIPTION: "Full system access and user management"
     },
     {
         RO_ID: 8002,
-        NAME: "Approver",
+        NAME: "Manager",
         DESCRIPTION: "Can view items and approve/decline requisitions"
     },
     {
         RO_ID: 8003,
-        NAME: "Administrator",
-        DESCRIPTION: "Full system access and user management"
+        NAME: "Encoder",
+        DESCRIPTION: "Can view items and create requisitions"
+    },
+    {
+        RO_ID: 8004,
+        NAME: "User",
+        DESCRIPTION: "Basic user access"
     }
 ];
 
