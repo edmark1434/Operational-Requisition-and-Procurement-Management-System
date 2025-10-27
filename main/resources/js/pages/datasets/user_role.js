008b00343110b03d4451be82bb732daa897c44e1
Rexshimura <mortpauljpm03@gmail.com>
2025-10-28 01:45:48 +0800

    user_role dataset

---
+++ main/resources/js/pages/datasets/user_role.js
@@ -0,0 +1,24 @@
+const userRoles = [
+    {
+        UR_ID: 5001,
+        US_ID: 2001,
+        RO_ID: 8001
+    },
+    {
+        UR_ID: 5002,
+        US_ID: 2002,
+        RO_ID: 8002
+    },
+    {
+        UR_ID: 5003,
+        US_ID: 2003,
+        RO_ID: 8003
+    },
+    {
+        UR_ID: 5004,
+        US_ID: 2001,
+        RO_ID: 8002
+    }
+];
+
+export default userRoles;
