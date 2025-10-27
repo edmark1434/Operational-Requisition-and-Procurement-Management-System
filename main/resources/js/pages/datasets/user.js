04c525e87f99618b601a99787b7e68c3ab071502
Rexshimura <mortpauljpm03@gmail.com>
2025-10-28 05:03:24 +0800

    sample users dataset

--- main/resources/js/pages/datasets/user.js
+++ main/resources/js/pages/datasets/user.js
@@ -1,21 +1,44 @@
+// user.js
 const users = [
     {
         US_ID: 2001,
+        FULLNAME: "John Doe",
         NAME: "john_doe",
         PASSWORD: "user123",
-        DATE_CREATED: "2024-01-01 00:00:00"
+        DATE_CREATED: "2024-01-01 00:00:00",
+        DATE_UPDATED: "2024-03-15 14:30:00"
     },
     {
         US_ID: 2002,
+        FULLNAME: "Maria Garcia",
         NAME: "maria_g",
         PASSWORD: "user456",
-        DATE_CREATED: "2024-01-02 00:00:00"
+        DATE_CREATED: "2024-01-02 00:00:00",
+        DATE_UPDATED: "2024-04-10 09:15:00"
     },
     {
         US_ID: 2003,
+        FULLNAME: "David Johnson",
         NAME: "david_j",
         PASSWORD: "user789",
-        DATE_CREATED: "2024-01-03 00:00:00"
+        DATE_CREATED: "2024-01-03 00:00:00",
+        DATE_UPDATED: "2024-02-20 16:45:00"
+    },
+    {
+        US_ID: 2004,
+        FULLNAME: "Sarah Wilson",
+        NAME: "sarah_w",
+        PASSWORD: "user101",
+        DATE_CREATED: "2024-01-04 00:00:00",
+        DATE_UPDATED: "2024-05-01 11:20:00"
+    },
+    {
+        US_ID: 2005,
+        FULLNAME: "Michael Brown",
+        NAME: "michael_b",
+        PASSWORD: "user112",
+        DATE_CREATED: "2024-01-05 00:00:00",
+        DATE_UPDATED: "2024-03-28 13:10:00"
     }
 ];
 
