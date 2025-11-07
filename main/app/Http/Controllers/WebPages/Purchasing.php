49cac145fd39ba26b2f41a4e413661e2977eaddf
Rexshimura <mortpauljpm03@gmail.com>
2025-11-08 02:57:54 +0800

    Updated Controller for Purchases

--- main/app/Http/Controllers/WebPages/Purchasing.php
+++ main/app/Http/Controllers/WebPages/Purchasing.php
@@ -11,7 +11,15 @@ class Purchasing extends Controller
     protected $base_path = "tabs/04-Purchases";
     public function index()
     {
-        return Inertia::render($this->base_path .'/purchases');
+        return Inertia::render($this->base_path .'/Purchases');
+    }
+    public function create(){
+        return Inertia::render($this->base_path .'/PurchaseOrderForm');
+    }
+    public function edit($id){
+        return Inertia::render($this->base_path .'/PurchaseOrderEdit', [
+            'purchaseId' => (int)$id
+        ]);
     }
 }
 
