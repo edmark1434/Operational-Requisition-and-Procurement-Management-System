ea8ad9f27b358a1c52e21354d77064c39e22fd74
Rexshimura <mortpauljpm03@gmail.com>
2025-11-10 05:30:27 +0800

    Supplier Controller Update

--- main/app/Http/Controllers/Supplier/SupplierController.php
+++ main/app/Http/Controllers/Supplier/SupplierController.php
@@ -9,6 +9,14 @@ class SupplierController extends Controller
     protected $base_path = "tabs/05-Suppliers";
     public function index()
     {
-        return Inertia::render($this->base_path .'/suppliers');
+        return Inertia::render($this->base_path .'/Suppliers');
+    }
+    public function store(){
+        return Inertia::render($this->base_path .'/SupplierAdd');
+    }
+    public function edit($id){
+        return Inertia::render($this->base_path .'/SupplierEdit', [
+            'supplierId' => (int)$id
+        ]);
     }
 }
