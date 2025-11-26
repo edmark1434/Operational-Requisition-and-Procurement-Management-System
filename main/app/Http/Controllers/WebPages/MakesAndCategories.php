7faf7f77496289e647427523b71e777a85e5ad63
Rexshimura <mortpauljpm03@gmail.com>
2025-11-27 03:12:05 +0800

    MAKES & CATEGORIES : Controller

--- main/app/Http/Controllers/WebPages/MakesAndCategories.php
+++ main/app/Http/Controllers/WebPages/MakesAndCategories.php
@@ -10,5 +10,21 @@ class MakesAndCategories extends Controller
     {
         return Inertia::render($this->base_path . '/Main');
     }
+    public function store_category(){
+        return Inertia::render($this->base_path .'/components/CategoryAdd');
+    }
+    public function edit_category($id){
+        return Inertia::render($this->base_path .'/components/CategoryEdit', [
+            'categoryId' => (int)$id
+        ]);
+    }
+    public function store_make(){
+        return Inertia::render($this->base_path .'/components/MakeAdd');
+    }
+    public function edit_make($id){
+        return Inertia::render($this->base_path .'/components/MakeEdit', [
+            'makeId' => (int)$id
+        ]);
+    }
 }
 
