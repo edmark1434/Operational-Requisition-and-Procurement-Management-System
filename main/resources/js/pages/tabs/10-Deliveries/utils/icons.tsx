5b3bf09e7c29635ab00808a40cffe48a42804025
Rexshimura <mortpauljpm03@gmail.com>
2025-11-27 21:15:44 +0800

    DELIVERIES: utils

--- main/resources/js/pages/tabs/10-Deliveries/utils/icons.tsx
+++ main/resources/js/pages/tabs/10-Deliveries/utils/icons.tsx
@@ -14,10 +14,5 @@ export const DeliveriesIcons = {
         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
         </svg>
-    ),
-    'in-transit': (
-        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
-        </svg>
     )
 };
